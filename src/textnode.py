c81e03c1d68a91e567bb0c40ae93fb4ea3f9960b
TepidMilk <ivorykingrey@gmail.com>
2024-12-13 14:10:02 -0600

    Add Test support and basic unit test

--- src/textnode.py
+++ src/textnode.py
@@ -15,14 +15,14 @@ class TextNode():
     text: string inline text content
     text_type: TextType  enum
     url: string url"""
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url = None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     #Returns true if all properties equal another TextNodes
-    def __eq__(self):
-        if (self.text, self.text_type, self.url) == (TextNode.text, TextNode.type, TextNode.url):
+    def __eq__(self, node2):
+        if (self.text, self.text_type, self.url) == (node2.text, node2.text_type, node2.url):
             return True
         
     #String representation of TextNode
