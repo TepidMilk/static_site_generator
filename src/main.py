a4075e806f66566cd59d88f436a05aeb442f258d
TepidMilk <ivorykingrey@gmail.com>
2024-12-13 15:56:28 -0600

    Edit Main.py to reflect type value

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 from textnode import *
 
-print("hello world")
-
 def main():
-    item = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+    item = TextNode("This is a text node", TextType.BOLD_TEXT.value, "https://www.boot.dev")    
 
-    print(item)
+    print(item.text_type)
 
 main()
