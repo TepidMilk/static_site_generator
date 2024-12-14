7588b99404fe6af636a783642430908b4f14a127
TepidMilk <ivorykingrey@gmail.com>
2024-12-13 20:43:14 -0600

    Edit main to test htmlnode repr method

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import *
+from htmlnode import *
 
 def main():
     item = TextNode("This is a text node", TextType.BOLD_TEXT.value, "https://www.boot.dev")    
-
-    print(item.text_type)
+    item2 = HTMLNode("a", "This is a link", None, {"href": "https://www.google.com", "target": "_blank",})
+    print(item)
+    print(item2)
 
 main()
