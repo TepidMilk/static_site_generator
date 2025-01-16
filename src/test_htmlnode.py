49a736c95dc1ae20e8ae3cf61fe29a46c4eadae3
TepidMilk <ivorykingrey@gmail.com>
2025-01-15 22:24:48 -0600

    Edit test_htmlnode to use self.assert

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,12 +2,10 @@ import unittest
 
 from htmlnode import HTMLNode
 
-run_cases = [{"href": "https://www.google.com", "target": "_blank",}, {"HREF": "HTTPS://WWW.GOOGLE.COM", "TARGET": "_BLANK",}, {"":"", " ":"",}, {"": " ", " ": " "}]
+run_cases = [{"href": "https://www.google.com", "target": "_blank",}, {"HREF": "HTTPS://WWW.GOOGLE.COM", "TARGET": "_BLANK",}, {" ":"", " ":"",}, {"": " ", " ": " "}]
 
 class TestHTMLNode(unittest.TestCase):
-    """First creates an HTMLNode to test. It then runs prop_to_html method.
-    this method should convert a dictionary into a single line string with it's key value pairs.
-    if successful returns True and if not False. If unable to run it raises an error to be read"""
+    """creates an HTMLNode and checks """
     def test_prop_to_html(self):
         try:
             for run in run_cases:
@@ -15,12 +13,8 @@ class TestHTMLNode(unittest.TestCase):
                 case_string = ""
                 for key in run:
                     case_string += f' {key}="{run[key]}"'
-                if html.prop_to_html() == case_string:
-                    print(html.prop_to_html())
-                    return True 
-                else:
-                    print(html.prop_to_html())
-                    return False
+                self.assertEqual(html.prop_to_html(), case_string)
+                
         except Exception as e:
             print(e)
 
