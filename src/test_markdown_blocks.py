9f39a563e591f05b96e2ccde96939526481a3d7a
TepidMilk <ivorykingrey@gmail.com>
2025-01-28 13:12:08 -0600

    Add tests for block_to_block_type
    Each test checks if block_to_block_type return
    value is the correct string

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -45,3 +45,63 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
             ],
             test
         )
+
+    def test_block_to_block_type(self):
+        block = "This is a paragraph"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "paragraph")
+
+    def test_block_type_heading_1(self):
+        block = "# This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+
+    def test_block_type_heading_2(self):
+        block = "## This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+
+    def test_block_type_heading_3(self):
+        block = "### This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+
+    def test_block_type_heading_4(self):
+        block = "#### This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+
+    def test_block_type_heading_5(self):
+        block = "##### This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+
+    def test_block_type_heading_6(self):
+        block = "###### This is a heading"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "heading")
+    
+    def test_block_type_code(self):
+        block = "```This is a block of code```"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "code")
+
+    def test_block_type_almost_code(self):
+        block = "```This is a block of code"
+        test = block_to_block_type(block)
+        self.assertNotEqual(test, "code")
+    
+    def test_block_type_quote(self):
+        block = "> This is a quote"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "quote")
+
+    def test_block_type_unordered_list(self):
+        block = "- this is an unordered list item - second item"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "unordered_list")
+
+    def test_block_type_ordered_list(self):
+        block = "1. This is an ordered list2. second item 3. third item"
+        test = block_to_block_type(block)
+        self.assertEqual(test, "ordered_list")
