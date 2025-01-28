73a6e133d9ea15783c8d1145d41dbae28a82dfcd
TepidMilk <ivorykingrey@gmail.com>
2025-01-28 13:10:41 -0600

    Add block_to_block_type function
    block_to_block_type takes a string of markdown
    text and returns a string representing its block
    type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,24 @@
-
+import re
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return list(map(lambda x: x.strip(), filter(None, blocks)))
+
+def block_to_block_type(block):
+    heading = r"^##?#?#?#?#?\s\w"
+    quote = r">"
+    unordered_list = r"-\s|\*\s"
+    ordered_list = r"\d\.\s"
+    code_start = r"^```"
+    code_end = r"```$"
+    if re.match(heading, block):
+        return "heading"
+    if re.match(quote, block):
+        return "quote"
+    if re.search(code_start, block) and re.search(code_end, block):
+        return "code"
+    if re.match(unordered_list, block):
+        return "unordered_list"
+    if re.match(ordered_list, block):
+        return "ordered_list"
+    return "paragraph"
