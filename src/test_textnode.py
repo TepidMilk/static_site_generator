f7d992a5f1847946652f63b1dc4c4899460f9166
TepidMilk <ivorykingrey@gmail.com>
2025-01-28 19:44:14 -0600

    Update test_textnode
    test_textnode now tests all cases of
    text_node_to_html_node previously only tested one

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -23,10 +23,42 @@ class TestTextNode(unittest.TestCase):
         text_node = TextNode("Hello, world!", TextType.NORMAL)
         html_node = text_node_to_html_node(text_node)
         expected = LeafNode(None, "Hello, world!")
-        try:
-            self.assertEqual(html_node, expected)
-        except Exception as e:
-            print(e)
+        self.assertEqual(html_node, expected)
+        
+    
+    def test_textnode_to_htmlnode_bold(self):
+        text_node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(text_node)
+        expected = LeafNode("b", "This is bold")
+        self.assertEqual(html_node, expected)
+        
+
+    def test_textnode_to_htmlnode_italic(self):
+        text_node = TextNode("This is Italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(text_node)
+        expected = LeafNode("i", "This is Italic")
+        self.assertEqual(html_node, expected)
+        
+
+    def test_textnode_to_htmlnode_code(self):
+        text_node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(text_node)
+        expected = LeafNode("code", "This is code")
+        self.assertEqual(html_node, expected)
+        
+    
+    def test_textnode_to_htmlnode_links(self):
+        text_node = TextNode("This is a link", TextType.LINKS, "https://www.google.com")
+        html_node = text_node_to_html_node(text_node)
+        expected = LeafNode("a", "This is a link", {"href": "https://www.google.com"})
+        self.assertEqual(html_node, expected)
+
+    def test_textnode_to_htmlnode_images(self):
+        text_node = TextNode("This is an image", TextType.IMAGES, "https://www.google.com")
+        html_node = text_node_to_html_node(text_node)
+        expected = LeafNode("img", "", {"src": "https://www.google.com", "alt": "This is an image"})
+        self.assertEqual(html_node, expected)
+    
         
 
 
