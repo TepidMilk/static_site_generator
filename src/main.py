32a7fb1d9a9ca297289b76f63711d0a49a92a042
TepidMilk <ivorykingrey@gmail.com>
2025-01-30 11:40:44 -0600

    Add copy_dir_to

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from textnode import *
 from htmlnode import *
-from enum import Enum
+from copy_dir_to import copy_dir_to
 
 def main():
-    item = TextNode("This is a text node", TextType.BOLD.value, "https://www.boot.dev")    
-    item2 = HTMLNode("a", "This is a link", None, {"href": "https://www.google.com", "target": "_blank",})
+    copy_dir_to("static", "public")
 
 main()
